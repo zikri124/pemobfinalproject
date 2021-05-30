6632b057b64345b4a428b0f35f9cec6a5afb4e94
Ilman <ilmannadhir99@gmail.com>
2021-05-30 18:31:38 +0700

    Various bug fix and some basic layout for the java

--- app/src/main/java/com/example/pemobfinalproject/register.java
+++ app/src/main/java/com/example/pemobfinalproject/register.java
@@ -2,13 +2,65 @@ package com.example.pemobfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class register extends AppCompatActivity {
+    ImageButton RegBack;
+    EditText RegUsername,RegEmail,RegPassword,RegTL;
+    TextView RegSignIn;
+    Button RegRegister;
+    Boolean TestNumber;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        RegBack = findViewById(R.id.backBtn);
+
+        RegUsername = findViewById(R.id.editTextUsername);
+        RegEmail = findViewById(R.id.editTextEmailAddress);
+        RegPassword = findViewById(R.id.editTextPassword);
+        RegTL = findViewById(R.id.editTextDate);
+
+        RegSignIn = findViewById(R.id.signInTxt);
+
+        RegRegister = findViewById(R.id.registerBtn);
+
+
+        RegBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent(register.this , landingPage.class));
+            }
+        });
+
+        RegSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(register.this, signIn.class));
+            }
+        });
+
+        RegRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (false){
+                // The Database Connection hasn't been connected this is just a basic layout
+                    Toast.makeText(getApplicationContext(),"Mohon Isi Semua Data!",Toast.LENGTH_LONG).show();
+                }
+                else
+                    startActivity(new Intent(register.this, mainMenu.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/pemobfinalproject/signIn.java
+++ app/src/main/java/com/example/pemobfinalproject/signIn.java
@@ -2,13 +2,57 @@ package com.example.pemobfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class signIn extends AppCompatActivity {
+    ImageButton SignBack;
+    EditText SignUsername,SignPassword;
+    TextView SignRegister;
+    Button SignIns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        SignBack = findViewById(R.id.backBtn);
+        SignUsername = findViewById(R.id.editTextUsername);
+        SignPassword =findViewById(R.id.editTextPassword);
+        SignRegister = findViewById(R.id.RegisterTXT);
+        SignIns = findViewById(R.id.signinBtn);
+
+        SignBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(signIn.this, landingPage.class));
+            }
+        });
+
+        SignRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(signIn.this, register.class));
+            }
+        });
+
+        SignIns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (false){
+                    // The Database Connection hasn't been connected this is just a basic layout
+                    Toast.makeText(getApplicationContext(),"Username atau Password Salah!",Toast.LENGTH_LONG).show();
+                }
+                else
+                startActivity(new Intent(signIn.this, mainMenu.class));
+            }
+        });
     }
 }
