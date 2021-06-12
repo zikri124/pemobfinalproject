ff531d86d1ff9eb75ba6453a8d4d7db0408f44b9
zikri124 <faisalsaja73@gmail.com>
2021-06-12 10:28:33 +0700

    Fitur select loc selesei

--- app/src/main/java/com/example/pemobfinalproject/selectLoc.java
+++ app/src/main/java/com/example/pemobfinalproject/selectLoc.java
@@ -1,26 +1,32 @@
 package com.example.pemobfinalproject;
 
 import android.content.Intent;
-import android.location.Location;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.mapbox.android.core.location.LocationEngine;
-import com.mapbox.android.core.location.LocationEngineCallback;
-import com.mapbox.android.core.location.LocationEngineProvider;
-import com.mapbox.android.core.location.LocationEngineRequest;
-import com.mapbox.android.core.location.LocationEngineResult;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.location.LocationComponent;
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
 import com.mapbox.mapboxsdk.location.modes.CameraMode;
@@ -29,25 +35,26 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
-import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
-import java.lang.ref.WeakReference;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class selectLoc extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener, View.OnClickListener {
+public class selectLoc extends AppCompatActivity implements PermissionsListener, View.OnClickListener, OnMapReadyCallback{
 
  private MapView mapView;
  private MapboxMap mapboxMap;
- private EditText textHospital, hospitalLoc;
+ private AutoCompleteTextView hospitalSelect;
  private ImageButton btnBack;
- private Button btnNext;
+ private Button btnNext, btnReset;
  private PermissionsManager permissionsManager;
- private String geojsonSourceLayerId = "geojsonSourceLayerId";
- private LocationEngine locationEngine;
- private long DEFAULT_INTERVAL_IN_MILLISECONDS = 1000L;
- private long DEFAULT_MAX_WAIT_TIME = DEFAULT_INTERVAL_IN_MILLISECONDS * 5;
 
- private MainActivityLocationCallback callback = new MainActivityLocationCallback(this);
+ private JSONObject dataJson;
+ private List<String> arr;
 
  @Override
  protected void onCreate(Bundle savedInstanceState) {
@@ -60,14 +67,16 @@ public class selectLoc extends AppCompatActivity implements OnMapReadyCallback, 
   mapView = findViewById(R.id.mapView);
   btnBack = findViewById(R.id.backBtn2);
   btnNext = findViewById(R.id.button_next_loc);
-  hospitalLoc = findViewById(R.id.hospitalLoc);
-  textHospital = findViewById(R.id.hospitalName);
+  btnReset = findViewById(R.id.button_reset);
+  hospitalSelect = (AutoCompleteTextView) findViewById(R.id.hospital_select);
 
   mapView.onCreate(savedInstanceState);
   mapView.getMapAsync(this);
 
   btnBack.setOnClickListener(this);
   btnNext.setOnClickListener(this);
+  btnReset.setOnClickListener(this);
+  getJsonData();
  }
 
  @Override
@@ -77,9 +86,112 @@ public class selectLoc extends AppCompatActivity implements OnMapReadyCallback, 
    @Override
    public void onStyleLoaded(@NonNull Style style) {
     enableLocationComponent(style);
-    setUpSource(style);
+    setMapClickListener();
    }
   });
+ }
+
+ private void searchHospital() {
+  arr = new ArrayList<>();
+  try {
+   JSONArray dataArray = dataJson.getJSONArray("features");
+   for (int i=0; i<dataArray.length(); i++) {
+    JSONObject res = dataArray.getJSONObject(i).getJSONObject("properties");
+    arr.add(res.getString("HOSPITAL_NAME")+", "+res.getString("ADDRESS_LINE1")+", "+res.getString("ADDRESS_LINE2")+", "+res.getString("CITY"));
+   }
+  } catch (JSONException e) {
+   e.printStackTrace();
+  }
+
+  ArrayAdapter<String> adapter = new ArrayAdapter<String>(selectLoc.this, android.R.layout.simple_dropdown_item_1line, arr);
+  hospitalSelect.setAdapter(adapter);
+  hospitalSelect.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+   @Override
+   public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+    try {
+     JSONArray dataArray = dataJson.getJSONArray("features");
+     for (int i2=0; i2<dataArray.length(); i2++) {
+      JSONObject res = dataArray.getJSONObject(i2).getJSONObject("properties");
+      String select = hospitalSelect.getText().toString();
+      if ((res.getString("HOSPITAL_NAME")+", "+res.getString("ADDRESS_LINE1")+", "+res.getString("ADDRESS_LINE2")+", "+res.getString("CITY")).equals(select)) {
+       JSONArray coor = dataArray.getJSONObject(i2).getJSONObject("geometry").getJSONArray("coordinates");
+       LatLng point2 = new LatLng();
+       point2.setLongitude(coor.getDouble(0));
+       point2.setLatitude(coor.getDouble(1));
+       animateCamera(point2);
+       break;
+      }
+     }
+    } catch (JSONException e) {
+     e.printStackTrace();
+    }
+   }
+  });
+ }
+
+ private void setMapClickListener() {
+  mapboxMap.addOnMapClickListener(new MapboxMap.OnMapClickListener() {
+   @Override
+   public boolean onMapClick(@NonNull @NotNull LatLng point) {
+    return handleClickIcon(point);
+   }
+  });
+ }
+
+ private boolean handleClickIcon(@NonNull LatLng point) {
+  //double currentZoomLevel = mapboxMap.getCameraPosition().zoom;
+  PointF screenPoint = mapboxMap.getProjection().toScreenLocation(point);
+
+  try {
+   JSONArray dataArray = dataJson.getJSONArray("features");
+   for (int i=0; i<dataArray.length(); i++) {
+    JSONArray res = dataArray.getJSONObject(i).getJSONObject("geometry").getJSONArray("coordinates");
+    LatLng point2 = new LatLng();
+    point2.setLongitude(res.getDouble(0));
+    point2.setLatitude(res.getDouble(1));
+    PointF screenPoint2 = mapboxMap.getProjection().toScreenLocation(point2);
+    if (screenPoint.x <= screenPoint2.x+40 && screenPoint.x >= screenPoint2.x-40) {
+     if (screenPoint.y <= screenPoint2.y+40 && screenPoint.y >= screenPoint2.y-40) {
+      Log.d("ClickInspect", "handleClickIcon: Clicked on "+dataArray.getJSONObject(i).getJSONObject("properties").getString("HOSPITAL_NAME"));
+      hospitalSelect.setText(arr.get(i));
+      animateCamera(point);
+      break;
+     }
+    }
+   }
+  } catch (JSONException e) {
+   e.printStackTrace();
+  }
+
+  return true;
+ }
+
+ private void animateCamera(LatLng point) {
+  CameraPosition position = new CameraPosition.Builder()
+          .target(point)
+          .zoom(14)
+          .build();
+  mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position),5000);
+ }
+
+ private void getJsonData() {
+  String url = "https://api.mapbox.com/datasets/v1/astrocat/ckpo0fnfn06mw28ltok0c27ne/features?access_token=pk.eyJ1IjoiYXN0cm9jYXQiLCJhIjoiY2twbGJ3c3ozMG83bTJwbmdqZmEzNXZrOSJ9.Jbr0rQWwtAiAJ7nrOlUJmQ";
+  RequestQueue queue = Volley.newRequestQueue(this);
+
+  JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+   @Override
+   public void onResponse(JSONObject response) {
+    dataJson = response;
+    searchHospital();
+   }
+  }, new Response.ErrorListener() {
+   @Override
+   public void onErrorResponse(VolleyError error) {
+    Log.d("ErrorU", "onResponse: " + error.toString());
+   }
+  });
+
+  queue.add(jsonObjectRequest);
  }
 
  @Override
@@ -113,11 +225,6 @@ public class selectLoc extends AppCompatActivity implements OnMapReadyCallback, 
   if (PermissionsManager.areLocationPermissionsGranted(this)) {
    LocationComponent locationComponent = mapboxMap.getLocationComponent();
 
-   LocationComponentActivationOptions locationComponentActivationOptions =
-           LocationComponentActivationOptions.builder(this, style)
-           .useDefaultLocationEngine(false)
-           .build();
-
    locationComponent.activateLocationComponent(LocationComponentActivationOptions.builder(this, style).build());
 
    locationComponent.setLocationComponentEnabled(true);
@@ -125,82 +232,10 @@ public class selectLoc extends AppCompatActivity implements OnMapReadyCallback, 
    locationComponent.setCameraMode(CameraMode.TRACKING);
 
    locationComponent.setRenderMode(RenderMode.COMPASS);
-
-   initLocationEngine();
   } else {
    permissionsManager = new PermissionsManager(this);
    permissionsManager.requestLocationPermissions(this);
   }
- }
-
- @SuppressWarnings("MissingPermission")
- private void initLocationEngine() {
-  locationEngine = LocationEngineProvider.getBestLocationEngine(this);
-
-  LocationEngineRequest request = new LocationEngineRequest.Builder(DEFAULT_INTERVAL_IN_MILLISECONDS)
-          .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
-          .setMaxWaitTime(DEFAULT_MAX_WAIT_TIME).build();
-
-  locationEngine.requestLocationUpdates(request, callback, getMainLooper());
-  locationEngine.getLastLocation(callback);
- }
-
- private static class MainActivityLocationCallback implements LocationEngineCallback<LocationEngineResult> {
-
-  private final WeakReference<selectLoc> activityWeakReference;
-
-  MainActivityLocationCallback(selectLoc activity) {
-   this.activityWeakReference = new WeakReference<>(activity);
-  }
-
-  /**
-   * The LocationEngineCallback interface's method which fires when the device's location has changed.
-   *
-   * @param result the LocationEngineResult object which has the last known location within it.
-   */
-  @Override
-  public void onSuccess(LocationEngineResult result) {
-   selectLoc activity = activityWeakReference.get();
-
-   if (activity != null) {
-    Location location = result.getLastLocation();
-
-    if (location == null) {
-     return;
-    }
-
-// Create a Toast which displays the new location's coordinates
-    /*Toast.makeText(activity, String.format(
-            String.valueOf(result.getLastLocation().getLatitude()), String.valueOf(result.getLastLocation().getLongitude())),
-            Toast.LENGTH_SHORT).show();
-    Log.d("loc", "lat: "+String.valueOf(result.getLastLocation().getLatitude())+" long: "+String.valueOf(result.getLastLocation().getLongitude()));
-     */
-
-// Pass the new location to the Maps SDK's LocationComponent
-    if (activity.mapboxMap != null && result.getLastLocation() != null) {
-     activity.mapboxMap.getLocationComponent().forceLocationUpdate(result.getLastLocation());
-    }
-   }
-  }
-
-  /**
-   * The LocationEngineCallback interface's method which fires when the device's location can not be captured
-   *
-   * @param exception the exception message
-   */
-  @Override
-  public void onFailure(@NonNull Exception exception) {
-   Log.d("LocationChangeActivity", exception.getLocalizedMessage());
-   selectLoc activity = activityWeakReference.get();
-   if (activity != null) {
-    Toast.makeText(activity, exception.getLocalizedMessage(),
-            Toast.LENGTH_SHORT).show();
-   }
-  }
- }
-
- private void setUpSource(@NonNull Style loadedMapStyle) {
-  loadedMapStyle.addSource(new GeoJsonSource(geojsonSourceLayerId));
  }
 
  @Override
@@ -250,6 +285,10 @@ public class selectLoc extends AppCompatActivity implements OnMapReadyCallback, 
   if (view.getId() == btnBack.getId()) {
    startActivity(new Intent(selectLoc.this, mainMenu.class));
    finishAffinity();
+  } else if (view.getId() == btnNext.getId()) {
+   startActivity(new Intent(selectLoc.this, selectTime.class));
+  } else if (view.getId() == btnReset.getId()) {
+   hospitalSelect.clearListSelection();
   }
  }
 }
